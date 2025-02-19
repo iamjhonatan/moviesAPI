936ebea67d7a2fc8a0f3a2ecd08b082b1c6883a0
iamjhonatan <jhonatanmedeiros7@gmail.com>
2025-02-18 21:32:17 -0300

    feat: add gitignore

--- MoviesAPI/obj/Debug/net9.0/MoviesAPI.AssemblyInfo.cs
+++ MoviesAPI/obj/Debug/net9.0/MoviesAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0ea81b2a02a5b0d2bdb0691f2adcc790bd15052")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29553f7eaf7542ed833f680390781e9703736839")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
