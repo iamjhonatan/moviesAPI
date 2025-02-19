29553f7eaf7542ed833f680390781e9703736839
iamjhonatan <jhonatanmedeiros7@gmail.com>
2025-02-18 21:21:49 -0300

    feat: add swagger in project

--- MoviesAPI/Program.cs
+++ MoviesAPI/Program.cs
@@ -10,6 +10,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "Movies API"));
 }
 
 app.UseHttpsRedirection();
